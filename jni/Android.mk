1048a0c3e32ea6f50e4c154506e34f2a230d37b1
Rangel Ivanov <rangelivanov88@gmail.com>
2013-01-04 15:11:02 +0200

    Removed android prebuild modules references.
    We are linking directly from the cegui output bin dir
    
    Since we are dynamically linking with iconv, iconv still needs to be an
    android prebuild module otherwise it wont get copied in the apropriate
    libs dir in the project. Will be fixed soon

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,24 +1,10 @@
-# Copyright (C) 2010 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := native-activity
 LOCAL_C_INCLUDES := include \
-        $(CEGUI_ROOT)/include
+        $(CEGUI_ROOT)/cegui/include
 
 LOCAL_SRC_FILES := src/main.cpp \
         src/CEGUITestApplication.cpp \
@@ -33,23 +19,20 @@ LOCAL_LDFLAGS	:= -Wl,-start-group
 LOCAL_CFLAGS := -DDEBUG 
 
 
-LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM -lfreetype 
+LOCAL_LDLIBS    := -L$(CEGUI_ROOT)/dependencies/lib/$(TARGET_ARCH_ABI) 
+LOCAL_LDLIBS    += -L$(CEGUI_ROOT)/systemlibs/$(TARGET_ARCH_ABI) 
+LOCAL_LDLIBS    += -L$(CEGUI_ROOT)/lib 
+LOCAL_LDLIBS    += -llog -landroid -lEGL -lGLESv1_CM 
+LOCAL_LDLIBS    += -lCEGUIOpenGLESRenderer_Static -lCEGUIBase_Static -lCEGUICoreWindowRendererSet_Static -lCEGUICommonDialogs_Static -lCEGUITinyXMLParser_Static -lCEGUIFreeImageImageCodec_Static 
+LOCAL_LDLIBS    += -ltinyxml -lfreetype -lFreeImage -lgnustl_static 
 
 LOCAL_CPP_FEATURES := rtti exceptions
 
-LOCAL_STATIC_LIBRARIES := CEGUIOpenGLESRenderer CEGUIBase  CEGUICoreWindowRendererSet CEGUICommonDialogs CEGUITinyXMLParser CEGUIFreeImageImageCodec android_native_app_glue freeimage
+LOCAL_STATIC_LIBRARIES := android_native_app_glue 
 
-LOCAL_SHARED_LIBRARIES := iconv tinyxml 
+LOCAL_SHARED_LIBRARIES := iconv
+
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
-$(call import-module,cegui-android-libs/cegui-base)
-$(call import-module,cegui-android-libs/opengles)
 $(call import-module,cegui-android-libs/iconv)
-$(call import-module,cegui-android-libs/tinyxml)
-$(call import-module,cegui-android-libs/free-image-codec)
-$(call import-module,cegui-android-libs/cegui-windows)
-$(call import-module,cegui-android-libs/freeimage)
-$(call import-module,cegui-android-libs/common-dialogs)
-$(call import-module,tinyxml/lib)
-
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,4 +1,4 @@
-APP_ABI := armeabi 
+APP_ABI := armeabi-v7a 
 APP_PLATFORM := android-9
 APP_STL := gnustl_static
 APP_OPTIM := debug
